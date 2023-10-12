0feb48390e98a6c82c0be0f6440636c12b0de0b6
guntar <guntar@me.com>
2023-10-12 09:44:18 +0700

    add temperature and crud

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import QuizMain from './components/Quiz/QuizMain.vue';
 import FormBinding from './components/FormBinding.vue';  
 import ListOfItem from './components/SimpleComponent/ListMain.vue';  
 import GridSortFilter from './components/GridSortFilter/GridSortFilterMain.vue';  
+import TemperatureConverter from './components/TemperatureConverter.vue';  
+import SimpleCRUD from './components/SimpleCRUD.vue';  
 
 const router = createRouter({
   history: createWebHistory(),
@@ -27,6 +29,14 @@ const router = createRouter({
     {
       path: '/grid-sort-filter',
       component: GridSortFilter
+    },
+    {
+      path: '/temperature-converter',
+      component: TemperatureConverter
+    },
+    {
+      path: '/simple-crud',
+      component: SimpleCRUD
     }
   ]
 });
